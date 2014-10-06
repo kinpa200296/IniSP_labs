a45b0f6a8524193fe7de2891cc46cc8dec22d2c3
kinpa200296 <kinpa200296@yandex.ru>
2014-10-06 12:35:17 +0300

    version 0.005 Changed periodical number conversion in ToString() from using Dictonary<T, T> to using HashSet<T> and List<T>

--- Kindruk.lab1/RationalNumber.cs
+++ Kindruk.lab1/RationalNumber.cs
@@ -251,25 +251,26 @@ namespace Kindruk.lab1
             if (wholePart == 0 && fractionalPart < 0)
                 sb.Insert(0, '-');
             sb.Append(cultureInfo.NumberFormat.NumberDecimalSeparator);
-            var nonFractionalPart = sb.Length;
             fractionalPart = Math.Abs(fractionalPart);
-            var dictionary = new Dictionary<long, long> {{fractionalPart, 1}};
+            var list = new List<long>();
+            var hashset = new HashSet<long>();
             while (fractionalPart > 0)
             {
                 fractionalPart *= 10;
-                if (dictionary.ContainsKey(fractionalPart))
-                {
-                    long extra;
-                    dictionary.TryGetValue(fractionalPart, out extra);
-                    nonFractionalPart += (int)extra;
-                    sb.Insert(nonFractionalPart, '(');
-                    sb.Append(')');
-                    return sb.ToString();
-                }
-                dictionary.Add(fractionalPart, sb.Length - nonFractionalPart);
-                sb.Append((fractionalPart/Denominator).ToString(formatProvider));
+                if (hashset.Contains(fractionalPart))
+                    break;
+                hashset.Add(fractionalPart);
+                list.Add(fractionalPart);
                 fractionalPart %= Denominator;
             }
+            foreach (var item in list)
+            {
+                if (item == fractionalPart)
+                    sb.Append('(');
+                sb.Append((item / Denominator).ToString(formatProvider));
+            }
+            if (hashset.Contains(fractionalPart))
+                sb.Append(')');
             return sb.ToString();
         }
 
